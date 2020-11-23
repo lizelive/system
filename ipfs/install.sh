2fa34c9e5f0c4052a9117614b9d2ae8853df9fe4
lizelive <github@lize.live>
2020-11-23 00:46:19 -0800

    Update install.sh

--- ipfs/install.sh
+++ ipfs/install.sh
@@ -6,6 +6,8 @@ ipfs_version=$(curl -s https://dist.ipfs.io/go-ipfs/versions | tail -1)
 tmp_dir=$(mktemp -d)
 cd $tmp_dir
 curl "https://dist.ipfs.io/go-ipfs/${ipfs_version}/go-ipfs_${ipfs_version}_linux-amd64.tar.gz" | tar -xvz
-bash go-ipfs/install.sh
+sudo bash go-ipfs/install.sh
 cd -
 rm -rf $tmp_dir
+
+# follow up with an ipfs init --profile=server,badgerds -e 
