cf7e74b7c5c1dcb6e3be0ec01657ccb9aa35e569
lizelive <github@lize.live>
2021-06-15 18:43:03 -0700

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -4,7 +4,7 @@ lsblk -I 8 -d
 
 # args
 DEVNAME=/dev/sda
-MOUNT_PATH=/datadrive
+MOUNT_PATH=/mnt/d
 
 PART_DEV=${DEVNAME}1
 
@@ -12,8 +12,8 @@ PART_DEV=${DEVNAME}1
 sudo parted ${DEVNAME} --script mklabel gpt mkpart xfspart xfs 0% 100%
 sudo mkfs.xfs ${PART_DEV}
 sudo partprobe ${PART_DEV}
-sudo mkdir /datadrive
-sudo chown $USER /datadrive
-sudo mount ${PART_DEV} /datadrive
+sudo mkdir ${MOUNT_PATH}
+sudo chown $USER ${MOUNT_PATH}
+sudo mount ${PART_DEV} ${MOUNT_PATH}
 eval blkid -o export ${PART_DEV}
 echo "UUID=${PARTUUID}  ${MOUNT_PATH}   xfs   defaults,nofail   1   2" | sudo tee -a /etc/fstab
